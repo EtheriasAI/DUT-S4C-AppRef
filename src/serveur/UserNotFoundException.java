97388128c93adce03eb46dc8a55b4da7ead59b09
EtheriasAI <72927555+EtheriasAI@users.noreply.github.com>
2021-03-21 17:47:53 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/EtheriasAI/AppRef into main""
    This reverts commit dc5207619003b06ad29998d19048b18c96055162.

--- src/serveur/UserNotFoundException.java
+++ src/serveur/UserNotFoundException.java
@@ -1,12 +1,19 @@
+/**
+ * Exception utilisé dans le cas où un utilisateur n'existe pas ou n'a pas été trouvé
+ * 
+ * @author GATIMEL CHARLES
+ * @author BARTHELME JUSTINE
+ * 
+ * @version 1.0
+ * 
+ */
 package serveur;
 
 public class UserNotFoundException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	//Message renvoyé dans l'exception lorsqu'il n'y a pas cet utilisateur
 	public UserNotFoundException(String message) {
 		super(message);
 	}
