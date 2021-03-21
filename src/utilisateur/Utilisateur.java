8d400fa20b27091fb124dbbe3798e4c6baa1a54c
Gatimelo <48652274+Gatimelo@users.noreply.github.com>
2021-03-22 03:35:00 +1100

    Update Utilisateur.java

--- src/utilisateur/Utilisateur.java
+++ src/utilisateur/Utilisateur.java
@@ -1,10 +1,19 @@
+/**
+ * interface qui sera implémenté par Programmeur et contient les méthodes nécessaires au programmeur
+ * 
+ * @author GATIMEL CHARLES
+ * @author BARTHELME JUSTINE
+ * 
+ * @version 1.0
+ * 
+ */
 package utilisateur;
 
 public interface Utilisateur {
 
-	String getLogin();
-	String getMDP();
-	String getURL();
-	void setURL(String url);
+	String getLogin();//Renvoyer le login
+	String getMDP();//Renvoyer le mdp
+	String getURL();//Renvoyer l'url
+	void setURL(String url);//Modifier l'url
 	
 }
